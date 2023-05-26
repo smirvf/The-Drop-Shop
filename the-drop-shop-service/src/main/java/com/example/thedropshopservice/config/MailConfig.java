14cc5331322cd3ecb20ee8ec5d917b4d7a1c4443
Roel Mclean <roel.mclean@outlook.com>
2023-05-26 23:09:46 +0100

    Small changes

--- the-drop-shop-service/src/main/java/com/example/thedropshopservice/config/MailConfig.java
+++ the-drop-shop-service/src/main/java/com/example/thedropshopservice/config/MailConfig.java
@@ -13,11 +13,11 @@ public class MailConfig {
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp-mail.outlook.com");
+        mailSender.setHost("");
         mailSender.setPort(587);
 
-        mailSender.setUsername("thedropshop.customerservice@outlook.com");
-        mailSender.setPassword("TemporaryPassword123");
+        mailSender.setUsername("");
+        mailSender.setPassword("");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
