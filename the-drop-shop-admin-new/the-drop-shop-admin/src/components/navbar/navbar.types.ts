1a1503e1e992f2ecda7d66289f11a73f73033ee7
Roel Mclean <roel.mclean@outlook.com>
2023-12-05 15:37:24 +0000

    remove navbar from admin
    - remove navbar from admin
    - initial skeleton of admin

--- the-drop-shop-admin-new/the-drop-shop-admin/src/components/navbar/navbar.types.ts
+++
@@ -1,6 +0,0 @@
-export interface NavBarInterface {
-    isTransparent?: boolean;
-    highlightedLink: Pages;
-}
-
-type Pages = "Home" | "Shop" | "About" | "Cart" | "Profile";
--- the-drop-shop-admin-new/the-drop-shop-admin/src/pages/index.js
+++ the-drop-shop-admin-new/the-drop-shop-admin/src/pages/index.js
@@ -1,15 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { NavBar } from '@/components/navbar/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <NavBar />
+      <h1>Netbuster Admin</h1>
     </>
   )
 }
